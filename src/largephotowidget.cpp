5beba80447b42bea654edf52554d198bb09da169
dean36963 <dean.birch0@gmail.com>
2015-04-12 22:57:48 +0100

    No idea why this was here

--- src/largephotowidget.cpp
+++ src/largephotowidget.cpp
@@ -1,15 +1,5 @@
 #include "largephotowidget.h"
 
-#include <libexif/exif-data.h>
-#include <libexif/exif-byte-order.h>
-#include <libexif/exif-data-type.h>
-#include <libexif/exif-ifd.h>
-#include <libexif/exif-log.h>
-#include <libexif/exif-tag.h>
-#include <libexif/exif-content.h>
-#include <libexif/exif-mnote-data.h>
-#include <libexif/exif-mem.h>
-
 LargePhotoWidget::LargePhotoWidget(QString photoPath, QWidget *parent) : QWidget(parent)
 {
     this->photoPath = photoPath;
